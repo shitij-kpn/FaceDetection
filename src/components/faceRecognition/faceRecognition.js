95ebd9e1bba0a10570c3568a8dd744b837a3d530
shitij <shitijtalan@gmail.com>
2019-08-16 01:17:11 +0530

    dry

--- src/components/faceRecognition/faceRecognition.js
+++ src/components/faceRecognition/faceRecognition.js
@@ -7,7 +7,7 @@ const FaceRecognition = ({imgURL , box}) => {
 		<div className='center ma '>
 			<div className='absolute mt2'>
 				<img id='inputImage' src={imgURL} alt="" width='500px' height = 'auto'/>
-				<div className='bounding-box' style = {{top : box.TopRow , right : box.RightCol , bottom : box.BottomRow , left :box.LeftCol}}></div>
+				<div className='bounding-box' style = {{top : box[1].TopRow , right : box[1].RightCol , bottom : box[1].BottomRow , left :box[1].LeftCol}}></div>
 			</div>
 		</div>
 	);
