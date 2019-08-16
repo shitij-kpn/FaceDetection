9202ca455427446633b949f71b275baaf0dc8355
shitij <shitijtalan@gmail.com>
2019-08-16 14:47:41 +0530

    dry

--- src/components/box/box.js
+++ src/components/box/box.js
@@ -3,6 +3,7 @@ import './box.css';
 
 
 const Box = (box) => {
+    console.table("BOX",box);
     return(
         <div className='bounding-box' style = {{top : box.TopRow , right : box.RightCol , bottom : box.BottomRow , left :box.LeftCol}}>
         </div>
